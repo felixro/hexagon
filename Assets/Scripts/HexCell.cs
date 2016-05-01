2bcf46b2fc4f9378c5d30b185481cc9832d7069b
Felix Rohrer <Felix.Rohrer@Sophos.com>
2016-05-01 14:55:46 -0400

    initial commit

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexCell : MonoBehaviour 
+{
+    public HexCoordinates coordinates;
+    public Color color;
+}
---
+++ Assets/Scripts/HexCoordinates.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class HexCoordinates
+{
+    [SerializeField]
+    private int x, z;
+
+    public int Y
+    {
+        get 
+        {
+            return -X - Z;
+        }
+    }
+
+    public int X 
+    {
+        get
+        {
+            return x;
+        }
+    }
+
+    public int Z
+    {
+        get
+        {
+            return z;
+        }
+    }
+
+    public HexCoordinates (int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+
+    public static HexCoordinates FromOffsetCoordinates(int x, int z)
+    {
+        return new HexCoordinates(x - z / 2, z);
+    }
+
+    public static HexCoordinates FromPosition(Vector3 position)
+    {
+        float x = position.x / (HexMetrics.innerRadius * 2f);
+        float y = -x;
+
+        float offset = position.z / (HexMetrics.outerRadius * 3f);
+        x -= offset;
+        y -= offset;
+
+        int iX = Mathf.RoundToInt(x);
+        int iY = Mathf.RoundToInt(y);
+        int iZ = Mathf.RoundToInt(-x - y);
+
+        if (iX + iY + iZ != 0)
+        {
+            int dX = Mathf.RoundToInt(x - iX);
+            int dY = Mathf.RoundToInt(y - iY);
+            int dZ = Mathf.RoundToInt(-x - y -iZ);
+
+            // reconstruct coordinates from the other two
+            if (dX > dY && dX > dZ)
+            {
+                iX = -iY -iZ;
+            }
+            else if(dZ > dY)
+            {
+                iZ = -iX - iY;
+            }
+        }
+
+        return new HexCoordinates(iX, iZ);
+    }
+
+    public override string ToString ()
+    {
+        return string.Format ("{0}, {1}, {2}", X, Y, Z);
+    }
+
+    public string ToStringOnSeparateLines ()
+    {
+        return string.Format ("{0}\n{1}\n{2}", X, Y, Z);
+    }
+}
